200c061286ce8acb5c110c8aafcba391144aba7f
Sergei Cherednichenko <sergeych92@gmail.com>
2021-01-21 18:30:10 -0500

    error handling decorator

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,5 +1,22 @@
 'use strict';
 
+
+angular
+    .module('myApp')
+    .decorator('$exceptionHandler', ['$delegate', ($delegate) => {
+      return function (exception, errorMessage) {
+          // If a promise is rejected (d.reject('abc')), then there are 2 params and the second will have the "possibly unhandled ..."
+          // Otherwise, if there is an exception inside of some .then(), then there will be just one param 
+          const strMessage = exception && errorMessage ? errorMessage : exception;
+          if ((strMessage.toString().toLowerCase()).startsWith("possibly unhandled rejection")) {
+              console.warn('CAUGHT!');
+              console.log(exception);
+          }
+          $delegate.apply(null, arguments);
+      };
+    }]);
+
+
 angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -9,60 +26,41 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
-  function showDiff(container) {
-    const expectedHeight = parseInt(container.dataset.height, 10);
-    const actualHeight = parseFloat(getComputedStyle(container).height);
-    console.log(`${container.getAttribute('id')} height diff: ${expectedHeight - actualHeight}`);
+.controller('View1Ctrl', ['$q', function($q) {
+  function okToGreet(name) {
+    return name === 'Robin Hood';
   }
 
-  // should return whether or not this element is a height friendly child -
-  // ((text OR inline OR other (script, comment)) AND has no non-compliant children)
-  function setFixedHeights(root) {
-    if ([Node.TEXT_NODE,
-      Node.COMMENT_NODE,
-      Node.PROCESSING_INSTRUCTION_NODE,
-      Node.CDATA_SECTION_NODE].includes(root.nodeType)) { 
-      return true;
-    }
-    if (root.nodeType === Node.ELEMENT_NODE) {
-      const style = getComputedStyle(root, null);
-      if (style.display === 'none') {
-        return true;
-      } else if (style.display === 'inline') {
-        // only allow if every child is a compliant child
-        return Array.from(root.childNodes).every(node => setFixedHeights(node));
-      } else { // can have its own height
-        if (root.hasChildNodes()) {
-          let allCompliant = true;
-          for (let child of root.childNodes) {
-            if (!setFixedHeights(child)) {
-              allCompliant = false;
-            }
-          }
-          if (allCompliant) { // if all children are OK, set a fixed height on root
-            const lineHeight = parseFloat(style.lineHeight);
-            if (!Number.isFinite(lineHeight)) {
-              throw new Error('Line height must be a decimal number. "normal" is not allowed.');
-            }
-            if (lineHeight === 0) { // shouldn't happen in real life unless there is some css style hack
-              throw new Error('Line height cannot be 0.');
-            }
-            const currentHeight = parseFloat(style.height);
-            const numOfLines = Math.round(currentHeight / lineHeight);
-            const forcedHeight = lineHeight * numOfLines;
-            root.style.height = forcedHeight.toFixed(1) + 'px';
-          }
-        }
-        return false;
+  function asyncGreet(name) {
+    var deferred = $q.defer();
+  
+    setTimeout(function() {
+      deferred.notify('About to greet ' + name + '.');
+  
+      if (okToGreet(name)) {
+        deferred.resolve('Hello, ' + name + '!');
+      } else {
+        deferred.reject('Greeting ' + name + ' is not allowed.');
       }
-    }
-    // Some other unexpected node type - error. Shouldn't happen outside of testing
-    throw new TypeError('This node type is not supported.');
+    }, 1000);
+  
+    return deferred.promise;
   }
-
-  setFixedHeights(
-    document.getElementById('simple')
-  );
-
+  
+  var promise = asyncGreet('Robin Hood');
+  promise
+    // .then(greeting => {
+    //   throw new Error('busted!');
+    // });
+    .then(function(greeting) {
+      console.log('Success: ' + greeting);
+    });
+    // throw new Error('fuck you');
+    // .then(function(greeting) {
+    //   console.log('Success: ' + greeting);
+    // }, function(reason) {
+    //   console.warn('Failed: ' + reason);
+    // }, function(update) {
+    //   console.info('Got notification: ' + update);
+    // });
 }]);
