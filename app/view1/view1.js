5b90430281fd7ac5118180c0c7c2f3d92981bc01
Sergei Cherednichenko <sergeych92@gmail.com>
2021-01-24 23:11:59 -0500

    alternative solution for 1.5.10

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,20 +1,21 @@
 'use strict';
 
+// 1.5.10
 
-angular
-    .module('myApp')
-    .decorator('$exceptionHandler', ['$delegate', ($delegate) => {
-      return function (exception, errorMessage) {
-          // If a promise is rejected (d.reject('abc')), then there are 2 params and the second will have the "possibly unhandled ..."
-          // Otherwise, if there is an exception inside of some .then(), then there will be just one param 
-          const strMessage = exception && errorMessage ? errorMessage : exception;
-          if ((strMessage.toString().toLowerCase()).startsWith("possibly unhandled rejection")) {
-              console.warn('CAUGHT!');
-              console.log(exception);
-          }
-          $delegate.apply(null, arguments);
-      };
-    }]);
+// angular
+//     .module('myApp')
+//     .decorator('$exceptionHandler', ['$delegate', ($delegate) => {
+//       return function (exception, errorMessage) {
+//           // If a promise is rejected (d.reject('abc')), then there are 2 params and the second will have the "possibly unhandled ..."
+//           // Otherwise, if there is an exception inside of some .then(), then there will be just one param 
+//           const strMessage = exception && errorMessage ? errorMessage : exception;
+//           if ((strMessage.toString().toLowerCase()).startsWith("possibly unhandled rejection")) {
+//               console.warn('CAUGHT!');
+//               console.log(exception);
+//           }
+//           $delegate.apply(null, arguments);
+//       };
+//     }]);
 
 
 angular.module('myApp.view1', ['ngRoute'])
@@ -26,41 +27,70 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', ['$q', function($q) {
-  function okToGreet(name) {
-    return name === 'Robin Hood';
-  }
+// Error Interceptor
+.factory('errorInterceptor', ['$q', ($q) => {
+  return {
+    response: function (response) {
+      return response;
+    },
+    responseError: function (response) {
+      return $q.reject(response);
+    }
+  };
+}])
+.config(['$httpProvider', ($httpProvider) => {
+  $httpProvider.interceptors.push('errorInterceptor');
+}])
 
-  function asyncGreet(name) {
-    var deferred = $q.defer();
-  
-    setTimeout(function() {
-      deferred.notify('About to greet ' + name + '.');
-  
-      if (okToGreet(name)) {
-        deferred.resolve('Hello, ' + name + '!');
-      } else {
-        deferred.reject('Greeting ' + name + ' is not allowed.');
-      }
-    }, 1000);
-  
-    return deferred.promise;
-  }
-  
-  var promise = asyncGreet('Robin Hood');
-  promise
-    // .then(greeting => {
-    //   throw new Error('busted!');
-    // });
-    .then(function(greeting) {
-      console.log('Success: ' + greeting);
+.controller('View1Ctrl', ['$q', '$http', function($q, $http) {
+  const apiKey = '16da40803f5f12b6407e4e7af5598e46' + 's';
+  $http.get(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}`)
+    .then(data => {
+      console.log('SUCCESS!');
+      console.log(data);
+    }, error => {
+      console.log('ERROR!');
+      console.log(error);
     });
-    // throw new Error('fuck you');
-    // .then(function(greeting) {
-    //   console.log('Success: ' + greeting);
-    // }, function(reason) {
-    //   console.warn('Failed: ' + reason);
-    // }, function(update) {
-    //   console.info('Got notification: ' + update);
-    // });
 }]);
+
+
+
+// .controller('View1Ctrl', ['$q', '$http', function($q, $http) {
+//   function okToGreet(name) {
+//     return name === 'Robin Hood';
+//   }
+
+//   function asyncGreet(name) {
+//     var deferred = $q.defer();
+  
+//     setTimeout(function() {
+//       deferred.notify('About to greet ' + name + '.');
+  
+//       if (okToGreet(name)) {
+//         deferred.resolve('Hello, ' + name + '!');
+//       } else {
+//         deferred.reject('Greeting ' + name + ' is not allowed.');
+//       }
+//     }, 1000);
+  
+//     return deferred.promise;
+//   }
+  
+//   var promise = asyncGreet('Robin Hood');
+//   promise
+//     .then(greeting => {
+//       throw new Error('busted!');
+//     });
+//     // .then(function(greeting) {
+//     //   console.log('Success: ' + greeting);
+//     // });
+//     // throw new Error('fuck you');
+//     // .then(function(greeting) {
+//     //   console.log('Success: ' + greeting);
+//     // }, function(reason) {
+//     //   console.warn('Failed: ' + reason);
+//     // }, function(update) {
+//     //   console.info('Got notification: ' + update);
+//     // });
+// }]);
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -8,7 +8,135 @@ angular.module('myApp.view2', ['ngRoute'])
     controller: 'View2Ctrl'
   });
 }])
+.run(myRun)
+.config(myConfig)
 
-.controller('View2Ctrl', [function() {
+.controller('View2Ctrl', ['$scope', '$http', function($scope, $http) {
+  $scope.get = function(validUrl, handleError) {
+    const apiKey = '16da40803f5f12b6407e4e7af5598e46' + (validUrl ? '' : 's');
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}`;
 
+    let errorCallback = function(response) {
+      alert('LOCAL errorCallback called');
+    };
+    if (!handleError) errorCallback = null;
+
+    $http.get(url).then(s => s).then(function(response) {
+      alert('Success callback called');
+    }, errorCallback);
+  }
 }]);
+
+
+function myRun($rootScope) {
+	$rootScope.$on('unhandledHttpError', function(event, response) {
+		alert('FALLBACK errorCallback called');
+    //TODO: replace previous line with actual fallback error handling
+	});
+}
+
+function hasCatch(promiseRoot) {
+  if (promiseRoot.$$state
+    && Array.isArray(promiseRoot.$$state.pending)
+    && promiseRoot.$$state.pending.length > 0) { // has child promises that have then-ed this one
+      const hasCatchArr = [];
+      for (let childTuple of promiseRoot.$$state.pending) { // pending is an array of child promises subscribed to this one
+        const childDeferred = childTuple[0];
+        const childError = childTuple[2];
+        hasCatchArr.push(
+          !!childError || hasCatch(childDeferred.promise)
+        );
+      }
+      if (hasCatchArr.every(s => s)) {
+        return true;
+      }
+  } else {
+    return false;
+  } 
+}
+
+//myConfig's only job is to fire unhandledHttpError event when an http request fails with no errorCallback specified
+function myConfig($provide) {
+	//START//
+  $provide.decorator('$http', function($delegate, $rootScope) {
+    var $http = $delegate;
+
+    var httpService = function(config) {
+      var promise = $http(config);
+      var childPromises = null;
+
+      promise.catch(err => {
+        const preudoPromise = {
+          $$state: {
+            pending: childPromises
+          }
+        };
+        if (!hasCatch(preudoPromise)) {
+          setTimeout(() => {
+            $rootScope.$emit('unhandledHttpError', err);
+          });
+        }
+      });
+
+      var promiseThen = promise.then;
+      promise.then = function(successCallback, errorCallback, notifyCallback) {
+        const v = promiseThen.call(promise, successCallback, errorCallback, notifyCallback);
+        childPromises = promise.$$state.pending;
+        return v;
+      };
+
+      return promise;
+    };
+    angular.extend(httpService, $http);
+
+    //handle shortcuts
+    httpService.get = function(url, config) {
+      config = config || {};
+      config.method = "GET";
+      config.url = url;
+      return httpService(config);
+    };
+    httpService.delete = function(url, config) {
+      config = config || {};
+      config.method = "DELETE";
+      config.url = url;
+      return httpService(config);
+    };
+    httpService.head = function(url, config) {
+      config = config || {};
+      config.method = "HEAD";
+      config.url = url;
+      return httpService(config);
+    };
+    httpService.jsonp = function(url, config) {
+      config = config || {};
+      config.method = "JSONP";
+      config.url = url;
+      return httpService(config);
+    };
+    httpService.post = function(url, data, config) {
+      config = config || {};
+      config.method = "POST";
+      config.url = url;
+      config.data = data;
+      return httpService(config);
+    };
+    httpService.put = function(url, data, config) {
+      config = config || {};
+      config.method = "PUT";
+      config.url = url;
+      config.data = data;
+      return httpService(config);
+    };
+    httpService.patch = function(url, data, config) {
+      config = config || {};
+      config.method = "PATCH";
+      config.url = url;
+      config.data = data;
+      return httpService(config);
+    };
+
+    return httpService;
+  });
+	//END//
+}
